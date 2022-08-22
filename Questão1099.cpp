275123825731e248580e2ae3f0b8d5b64495d0be
pedroevaristo <hattoriminamoto@gmail.com>
2022-08-22 16:11:10 -0300

    22-Ago-22

--- Questão1099.cpp
+++ Questão1099.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 
-void verifynumber(int);
+void verifynumber(int input[], int num);
 
 using namespace std;
 
@@ -16,7 +16,8 @@ int main()
     loop = 0;
     while (loop < amount)
     {
-        int loop1 = 0, aux;
+        int loop1 = 0, aux, total = 0;
+
         do
         {
             cin >> input[loop1];
@@ -24,32 +25,55 @@ int main()
             loop1 += 1;
 
         } while (loop1 < 2);
-        /*aux = 0;
+
+        aux = 0;
+
         while (input[0] > input[1])
         {
             aux = input[0];
             input[0] = input[1];
             input[1] = aux;
-        }*/
-    
-        for (int check = input[0]; check < amount; check++)//fazer de alguma forma que ele comece a conta apartir de 4 a 5 e dizer que o resultado é 0
-        {
-            int total = input[0]+input[1];
-            cout<<total<<endl;
         }
+
+        total = abs(input[0] - input[1]);
+
+        cout << total << endl;
+
+        if (total <= 0)
+        {
+            cout << "Resultado:" << total << endl;
+            break;
+        }
+
+        int check, repeat = 0, count = 0;
+
+        for (check = 0; check < total; check++)
+        {
+            input[0] += 1;
+            if (input[0] % 2 == 1)
+            {
+                repeat += input[0];
+                count += 1;
+            }
+        }
+
+        if (repeat < input[1])
+        { //|| repeat >=input[1]
+            cout << "Resultado: " << repeat << endl;
+            
+            
+        }
+        /*if (count >= 2)
+        {
+            cout<<"dentro do count: "<<repeat<<endl; TO tentnado de todas as formas possiveis com cin>> 3 e 8 e fica dando erro de logica 
+            
+        }*/
+        else
+        {
+            cout << "resultado = " << repeat - input[1] << endl;
+            
+        }
+
         loop += 1;
     }
 }
-/*
-aux+=1;
-if(input[amount] == input[amount]||aux <= 1 ){
-        cout<<input[amount]<<" "<<input[amount]<<"result: 0"<<endl;
-        break
-       }
-
- */
-/* do
-         {
-             cin >> input[loop];
-             loop += 1;
-         } while (loop < input[amount]);*/
