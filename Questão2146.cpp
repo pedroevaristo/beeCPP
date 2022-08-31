cdcc8c3650a6e42778eb88aeb1cad6298d9414e7
pedroevaristo <hattoriminamoto@gmail.com>
2022-08-31 15:05:16 -0300

    31.08.22

---
+++ Questão2146.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int array[1], loop;
+    loop = 0;
+    do
+    {
+        cin >> array[0];
+        while (array[0] < 1001)
+        {
+            cout << "Digite novamente" << endl;
+            cin >> array[0];
+        }
+        int total = 0;
+        //cout<<array[1]<<endl;
+        total = abs((array[0]- 1));
+        cout << total << endl;
+        array[0] = 0;
+        loop += 1;
+    } while (loop > 1);
+}
