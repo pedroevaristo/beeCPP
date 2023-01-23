d74af87800a217bd911aa6d17c9ee74b8968f98b
pedroevaristo <hattoriminamoto@gmail.com>
2023-01-23 15:21:57 -0300

    23/01/2023

--- Questão1042.cpp
+++ Questão1042.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    // float takeNumbers[3];
+    float takeNumbers[3];
     float storeNumbers[3];
 
     int loopToTakeInfor = 0;
@@ -13,20 +13,36 @@ int main()
     do
     {
         cin >> storeNumbers[loopToTakeInfor];
-
+        takeNumbers[loopToTakeInfor] = storeNumbers[loopToTakeInfor];
         loopToTakeInfor += 1;
     } while (loopToTakeInfor < 3);
 
-    int auxliar = 0;
+    int auxiliar = 0;
     for (int loopOrganising1 = 0; loopOrganising1 < 3; loopOrganising1++)
     {
         for (int loopOrganising2 = 0; loopOrganising2 < 3; loopOrganising2++)
         {
-            if (storeNumbers[loopOrganising1] > storeNumbers[loopOrganising2])
+            if (storeNumbers[loopOrganising1] < storeNumbers[loopOrganising2])
             {
-
-                
+                auxiliar = storeNumbers[loopOrganising1];
+                storeNumbers[loopOrganising1] = storeNumbers[loopOrganising2];
+                storeNumbers[loopOrganising2] = auxiliar;
             }
         }
+    }
+    cout << "\n"
+         << endl;
+    int loopWhile = 0;
+    while (loopWhile < 3)
+    {
+
+        cout << storeNumbers[loopWhile] << endl;
+        loopWhile += 1;
+    }
+    cout << "\n"
+         << endl;
+    for (int loopFor = 0; loopFor < 3; loopFor++)
+    {
+        cout << takeNumbers[loopFor] << endl;
     }
 }
