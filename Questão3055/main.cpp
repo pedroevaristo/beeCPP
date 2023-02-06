9febf6f110f68fdf49ad21431c9f220be67c5032
pedroevaristo <hattoriminamoto@gmail.com>
2023-02-06 19:07:31 -0300

    6/2/2023

--- Questão3055/main.cpp
+++ Questão3055/main.cpp
@@ -1,37 +1,9 @@
-#include <iostream>
-#include <cstdlib>
-#include "header.h"
-
+#include<iostream>
+#include<csdtlib>
 using namespace std;
 
-void receiveInput();
 
-int main()
-{
+int main(){
 
-    student inforPersonalStudent;
-    int quantityStudent;
-
-    cout << "Digite a(s) nota(s) do(s) aluno(s):" << endl;
-
-    cin >> quantityStudent;
-
-    int loopWhile = 0;
-
-    do
-    {
-        // receiveInput();
-        cout << "Digite o nome do aluno: ";
-        cin >> inforPersonalStudent.nameStudent;
-
-        cout << "A nota 1: ";
-        cin >> inforPersonalStudent.gradeStudent;
-
-        cout << "digite a media: ";
-        cin >> inforPersonalStudent.mediaStudnet;
-
-        loopWhile++;
-    } while (loopWhile < quantityStudent);
-
-    // cout<<inforPersonalStudent.nameStudent<<endl;
+    
 }
