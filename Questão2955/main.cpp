36759f4ea71a3c1cb69acfb4b99f91256b1816a6
pedroevaristo <hattoriminamoto@gmail.com>
2023-02-17 22:39:03 -0300

    17/02/2023

---
+++ Questão2955/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include "head.h"
+using namespace std;
+
+/*Tenho que saber usar struct para armazenar as informações e mostrar-las*/
+int main()
+{
+  
+    /* 
+    do
+    {
+        cin >> p.Numbers[count];
+        count++;
+
+    } while (count < 3);
+
+    for(int c = 0;c < 3; c++){
+        cout<<p.Numbers[c]<<endl;
+    }*/
+}
